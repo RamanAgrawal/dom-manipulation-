864c5b4c99f0818035445c464881af41fc2a4583
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-02-06 22:19:38 +0530

    creating js file

---
+++ dom.js
@@ -0,0 +1,6 @@
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+console.log("madarchod");
+let add= document.querySelector('.title')
+add.style.fontWeight='bold';
+add.style.color='green'
